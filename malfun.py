13026a55d6db758cbd1c59d10ec3e7e103be1caa
zipeks <matzielinski7@gmail.com>
2022-09-18 13:53:28 +0200

    more

--- malfun.py
+++ malfun.py
@@ -6,7 +6,7 @@ import time
 import re
 def get_titles(a):
     f = open("titles2.txt", "a")
-    for n in range(100,a+1):
+    for n in range(550,a+1):
         print(n)
         respone =  requests.get(f'https://api.jikan.moe/v4/top/anime?page={n}&filter=bypopularity')
 
@@ -21,15 +21,15 @@ def get_titles(a):
         time.sleep(1.01)
 
     f.close()
-# get_titles(250)
+# get_titles(700)
 
 def search():
     with open('titles.txt', 'r') as titles:
         for line in titles:
             line = line.rstrip()
             table = line.split(" ")
-            if re.search("^[wca]", table[0], re.I):
-               if len(table)>1 and re.search("^[wca]", table[1], re.I):
-                    if len(table)>2 and re.search("^[wca]", table[2], re.I):
+            if re.search("^[sca]", table[0], re.I):
+               if len(table)>1 and re.search("^[sca]", table[1], re.I) and len(table):
+                    if len(table)>2 and re.search("^[sca]", table[2], re.I) and len(table)<4:
                         print(line)
 search()
